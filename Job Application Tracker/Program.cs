c64e4917f39a05d0e74db8634a4aea57ad4ef488
gudenamrutha <gudenamrutha@gmail.com>
2026-06-04 05:20:58 +0200

    Updated Cors

--- Job Application Tracker/Program.cs
+++ Job Application Tracker/Program.cs
@@ -66,7 +66,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173",
+            "https://job-seeker-job-tracker.netlify.app")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
